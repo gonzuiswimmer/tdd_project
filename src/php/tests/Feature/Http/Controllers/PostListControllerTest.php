f41c324ad2419447a79d99a48eb557dfc1df5097
gonzuiswimmer <gonzuiswimmer@gmail.com>
2023-07-10 16:14:17 +0900

    top画面にブログ一覧が表示されるテストを実装。また、各レイアウトファイルやcssファイルを追加

--- src/php/tests/Feature/Http/Controllers/PostListControllerTest.php
+++ src/php/tests/Feature/Http/Controllers/PostListControllerTest.php
@@ -4,12 +4,27 @@ namespace Tests\Feature\Http\Controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Post;
 use Tests\TestCase;
 
 class PostListControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_top画面にアクセスするとブログ一覧ページが表示される()
     {
-        $this->get('/')->assertOk();
+        // 準備
+        $post1 = Post::factory()->create([
+            'title' => 'テストタイトル1'
+        ]);
+        $post2 = Post::factory()->create([
+            'title' => 'テストタイトル2'
+        ]);
+        // 実行
+        // 検証
+        $this->get('/')
+        ->assertOk()
+        ->assertSee('テストタイトル1')
+        ->assertSee('テストタイトル2');
     }
 }
